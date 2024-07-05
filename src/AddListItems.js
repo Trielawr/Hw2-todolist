60899a614b9c1252e53279b1a022d1d40775cb3f
Lelihelija <163314878+Helga-UA@users.noreply.github.com>
2024-07-05 20:36:27 +0300

    code styling

--- src/AddListItems.js
+++ src/AddListItems.js
@@ -3,8 +3,9 @@ import React from "react";
 const AddListItems = (props) => {
     return (
         <>
-            <li key={`${props.element}`}>{props.element}</li>
+            <li key={`${ props.element }`}>{ props.element }</li>
         </>
     )
 };
+
 export default AddListItems;
