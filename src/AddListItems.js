217ea8b3f0d05c192567e68a21a203348b10ab88
Lelihelija <163314878+Helga-UA@users.noreply.github.com>
2024-07-23 17:02:16 +0300

    Update AddListItems.js

--- src/AddListItems.js
+++ src/AddListItems.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-const AddListItems = (props) => {
+const AddListItems = ({ element, id, onDeleteClick }) => {
     return (
         <>
-            <li key={`${ props.element }`}>{ props.element }</li>
+            <li key={`${ props.element }`}>
+                { props.element }
+                <button type="button" onClick={ onDeleteClick(id) }>Delete</button>
+            </li>
         </>
     )
 };
+
 export default AddListItems;
