1ebfbab384643c271af4b7ba7432d7d4bc0e6d37
Trielawr <trielawr@gmail.com>
2024-08-03 00:32:19 +0300

    HomeWork 4 Final
    Завдання:
    1) Переписати наш туду ліст на классовий компонент
    2) Зберігати данні у loaclStorage
    4) Додати кнопку по якій наш localStorage буде очищуватись (Clear Todo List) /*Зробила ще очищення всього листа так як на мене це випливає з назви кнопки*/

--- src/AddListItems.js
+++ src/AddListItems.js
@@ -1,10 +1,10 @@
-import React, { Children } from "react";
+import React from "react";
 
 const AddListItems = (props) => {
     return (
         <>
-            <li key={props.id}>{props.element}</li>
-            {props.children}
+            <li key={ props.id }>{ props.element }</li>
+            { props.children }
          </>
     )
 };
--- src/ClassComponent.js
+++ src/ClassComponent.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react'
 import AddListItems from './AddListItems';
 import ButtonComponent from './ButtonComponent';
 
-
-
 class ClassComponent extends Component {
     state = {
         uuid: 0,
@@ -11,39 +9,55 @@ class ClassComponent extends Component {
         input: "new task",
     }
 
-    onChangeHandler =(e) => {
-        const value = e.target.value;
-           this.setState({ input: value });
-            
+    componentDidMount() {
+        const locStorage = localStorage.getItem("todos");
+        if (locStorage) {
+            this.setState({todos: JSON.parse(locStorage)}); 
+        }
     }
 
-    onClickHandler = (e) => {
-        this.setState({ uuid: crypto.randomUUID()});
+    componentDidUpdate(prevState) {
+        if (prevState.todos !== this.state.todos) {
+            localStorage.setItem("todos", JSON.stringify(this.state.todos));
+        }
+    }
+
+    onChangeHandler = (e) => {
+        const value = e.target.value;
+           this.setState({ input: value });     
+    }
+
+    onClickHandler = () => {
+        this.setState({ uuid: crypto.randomUUID() });
         this.setState({ todos: [...this.state.todos, {id: this.state.uuid, todo: this.state.input}] });
         this.setState({ input: "new task" });
     }
 
     onDeleteHandler = (id) => {
-        console.log(this.state.todos.id);
-        const newTodos = this.state.todos.filter(id => this.state.todos.id !== id);
+        const newTodos = this.state.todos.filter(element => element.id !== id);
         this.setState({ todos: newTodos });
+    }
+
+    componentWillUnmount() {
+        localStorage.clear();
+        this.setState({ todos: [] });
     }
 
     render() {
        return (
       <>
-          <input value={this.state.input} onChange={this.onChangeHandler}/> 
-               <p>{this.state.todos.length}</p>
-               {this.state.todos.map(element => <AddListItems key={element.id} id={element.id} element={element.todo}>
-                   <ButtonComponent type={"button"} text={"Delete"} onClick={()=>this.onDeleteHandler(element.id)} />
-               </AddListItems>)}
+          <input value={ this.state.input } onChange={ this.onChangeHandler }/> 
+               <p>{ this.state.todos.length }</p>
+               { this.state.todos.map(element => <AddListItems key={ element.id } id={ element.id } element={ element.todo }>
+                   <ButtonComponent type={ "button" } text={ "Delete" } onClick={ ()=>this.onDeleteHandler(element.id) } />
+               </AddListItems>) }
+               <br />
+               <ButtonComponent type={ "button" } text={ "Add Todo" } onClick={ this.onClickHandler } />
                <br/>
-               <button type="button" onClick={ this.onClickHandler }> Add Todo</button>
+               <ButtonComponent type={ "button" } text={ "Clear Todo List" } onClick={ ()=>this.componentWillUnmount() } />
           </>
-  )
-
+        )
     }
- 
 }
 
 export default ClassComponent;
